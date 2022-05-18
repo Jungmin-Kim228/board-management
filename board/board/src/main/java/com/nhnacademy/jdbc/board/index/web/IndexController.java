165239f97d788e43f067f8756fec7fe01d99297b
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-05-18 19:47:46 +0900

    thymeleaf적용시도중

--- board/board/src/main/java/com/nhnacademy/jdbc/board/index/web/IndexController.java
+++ board/board/src/main/java/com/nhnacademy/jdbc/board/index/web/IndexController.java
@@ -1,49 +1,17 @@
 package com.nhnacademy.jdbc.board.index.web;
 
-import com.nhnacademy.jdbc.board.user.domain.User;
-import com.nhnacademy.jdbc.board.user.service.UserService;
-import com.nhnacademy.jdbc.board.user.service.impl.DefaultUserService;
-import java.util.Objects;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 @Slf4j
 public class IndexController {
-    private final UserService userService;
-
-    public IndexController(DefaultUserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping(value = {"/","/index.nhn"})
-    public String index(HttpServletRequest req) {
-        HttpSession session = req.getSession(false);
-        if(Objects.isNull(session)) {
-            return "login/loginForm";
-        }
+    public String index() {
         return "index/index";
-    }
-
-    @PostMapping("/")
-    public String login(@RequestParam("logId") String id,
-                        @RequestParam("logPw") String pw,
-                        HttpServletRequest req) {
-        if (userService.successLogin(id, pw)) {
-            req.getSession().setAttribute("id", id);
-            return "redirect:/";
-        }
-        req.getSession().invalidate();
-        return "login/loginForm";
     }
 }
---
+++ board/board/src/main/java/com/nhnacademy/jdbc/board/index/web/LoginControlLer.java
@@ -0,0 +1,40 @@
+package com.nhnacademy.jdbc.board.index.web;
+
+import com.nhnacademy.jdbc.board.user.service.UserService;
+import com.nhnacademy.jdbc.board.user.service.impl.DefaultUserService;
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginControlLer {
+    private final UserService userService;
+
+    public LoginControlLer(DefaultUserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/login")
+    public String loginCheck(HttpServletRequest req) {
+        HttpSession session = req.getSession(false);
+        if(Objects.isNull(session)) {
+            return "login/loginForm";
+        }
+        return "/";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("logId") String id,
+                        @RequestParam("logPw") String pw,
+                        HttpServletRequest req) {
+        if (userService.successLogin(id, pw)) {
+            req.getSession().setAttribute("id", id);
+            return "redirect:/";
+        }
+        req.getSession().invalidate();
+        return "login/loginForm";
+    }
+}
