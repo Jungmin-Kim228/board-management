70caa99486dd6ffb91cc3f5e578f8d2f0ed79313
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-05-19 10:56:46 +0900

    html추가

--- board/board/src/main/java/com/nhnacademy/jdbc/board/compre/mapper/PostMapper.java
+++ board/board/src/main/java/com/nhnacademy/jdbc/board/compre/mapper/PostMapper.java
@@ -3,7 +3,9 @@ package com.nhnacademy.jdbc.board.compre.mapper;
 import com.nhnacademy.jdbc.board.compre.domain.Post;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PostMapper {
 
     Optional<Post> selectPost(int id);
--- board/board/src/main/java/com/nhnacademy/jdbc/board/controller/BoardController.java
+++ board/board/src/main/java/com/nhnacademy/jdbc/board/controller/BoardController.java
@@ -1,13 +1,35 @@
 package com.nhnacademy.jdbc.board.controller;
 
+import com.nhnacademy.jdbc.board.compre.domain.Post;
+import com.nhnacademy.jdbc.board.compre.service.PostService;
+import com.nhnacademy.jdbc.board.compre.service.impl.DefaultPostService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+    private final PostService postService;
+
+    public BoardController(DefaultPostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/board")
-    public String boardView() {
+    public String boardView(Model model) {
+        List<Post> posts = postService.getPosts();
+        model.addAttribute("allPost", posts);
         return "board/boardView";
+    }
+
+    @GetMapping("/boardRegister")
+    public String boardRegister() {
+        return "board/boardRegister";
+    }
+
+    @GetMapping("/boardModify")
+    public String boardModify() {
+        return "board/boardModify";
     }
 }
