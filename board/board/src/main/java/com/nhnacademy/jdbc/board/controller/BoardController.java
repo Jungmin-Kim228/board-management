245a8f97f696e1ef55dcb52945fce3bfe05129ed
dgb980228 <dgb980228@naver.com>
2022-05-20 10:54:05 +0900

    tyring pagination html, sql, mapper

--- board/board/src/main/java/com/nhnacademy/jdbc/board/controller/BoardController.java
+++ board/board/src/main/java/com/nhnacademy/jdbc/board/controller/BoardController.java
@@ -35,7 +35,7 @@ public class BoardController {
     @GetMapping("/board")
     public String boardView(Model model, @RequestParam(value = "page", defaultValue = "1") final int page) {
         Pagination pagination = new Pagination(postService.getCount(), page);
-        List<PostDAO> posts = postService.getListPage(pagination) ;
+        List<PostDAO> posts = postService.getListPage(pagination);
 
         model.addAttribute("allPost", posts);
         model.addAttribute("page", page);
